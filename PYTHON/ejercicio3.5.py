a04318b5190e76efc89d17ada1f6589f38335b53
Narx.365 <narcosixj.r@gmail.com>
2025-10-07 14:16:08 +0200

    ejercicios python hasta apuntes nº3

---
+++ PYTHON/ejercicio3.5.py
@@ -0,0 +1,4 @@
+num1=int(input("dame el primer numero: "))
+num2=int(input("dame el segundo numero: "))
+media=(num1+num2)/2
+print("la media de tus numeros es " ,media)
---
+++ PYTHON/python1.py
@@ -0,0 +1,10 @@
+nombre= "pedro"
+print("mi nombre es" ,nombre)
+edad=17
+print("mi edad es" ,edad)
+ciudad="teruel"
+print("mi ciudad es" ,ciudad)
+habitantes=30000
+print("los habitantes de " ,ciudad, " son " ,habitantes, ".")
+nombreAlumno= input ("escribe tu nombre: ")
+print( nombreAlumno )

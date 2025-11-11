65e6886b531ee1ec74470882835a9017eaccbe4d
Narx.365 <narcosixj.r@gmail.com>
2025-11-11 14:16:38 +0100

    correccion ejercicio python

--- PYTHON/ejercicios condicionales/ejercicioC3.py
+++ PYTHON/ejercicios condicionales/ejercicioC3.py
@@ -1,11 +1,14 @@
-num1=int(input("dame un numero: "))
-num2=int(input("dame otro numero: "))
+num1=float(input("dame un numero: "))
+num2=float(input("dame otro numero: "))
 if (num1>num2) :
     print(num1, " + " ,num2, " = " ,num1+num2)
     print(num1, " - " ,num2, " = " ,num1-num2)
 elif (num1<num2) :
-    print(num1, " * " ,num2, " = " ,num1*num2)
-    print(num1, " / " ,num2, " = " ,num1/num2)
+    if (num2 != 0) :
+        print(num1, " * " ,num2, " = " ,num1*num2)
+        print(num1, " / " ,num2, " = " ,num1/num2)
+    else: 
+        print("esto es una incongruencia, vuelve a integrar tus numeros")
 else:
     print("pndejo d mierda, los dos numeros son iguales, andate a la mrda")
     
